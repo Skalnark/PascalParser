725a3e1e5955df680d4fcabed577d19208e0a024
Skalnark <skalnark@live.com>
2019-06-24 17:50:42 -0300

    lexer

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,10 @@ import System.IO
 main :: IO ()
 main = do
        putStrLn "Iniciando..."
-       source <- readFile "files/source.txt"
-       writeFile "files/table.txt" (writeTokens source)
+       source1 <- readFile "files/meu_codigo.txt"
+       source2 <- readFile "files/codigo_professor1.txt"
+       source3 <- readFile "files/codigo_professor2.txt"
+       writeFile "files/tabela_meu_codigo.txt" (writeTokens source1)
+       writeFile "files/tabela_codigo_professor1.txt" (writeTokens source2)
+       writeFile "files/tabela_codigo_professor2.txt" (writeTokens source3)
        putStrLn "Escrita realizada com sucesso"
--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,5 +1,5 @@
-module Lexer(Line, Content, Delimiter, Keyword, Identifier,
-             Token(..), Type(..), lexicalAnalysis, showToken, writeTokens, mapType)
+module Lexer(Line, Content, Delimiter, Keyword, Identifier, Token(..), Type(..), 
+             lexicalAnalysis, showToken, writeTokens, mapType)
   where
   
   import Data.Maybe
@@ -10,28 +10,16 @@ module Lexer(Line, Content, Delimiter, Keyword, Identifier,
   type Delimiter  = String
   type Keyword    = String
   type Identifier = String
-  data Token = Token (Content, Type, Line)
+  data Token = Token (Content, Maybe Type, Line)
 
-  data Type = Null
-            | Delimiter 
-            | Keyword 
-            | Identifier 
-            | RelOperator
-            | AddOperator
-            | MulOperator
-            | Assignment
-            | IntegerNumber
-            | RealNumber
+  data Type = Null | Delimiter | Keyword | Identifier | RelOperator | AddOperator
+            | MulOperator | Assignment | IntegerNumber | RealNumber
       deriving(Eq, Show)
 
-  reservedNames = ["program", "var", "integer", "real", "bolean",
-                   "procedure", "begin", "end", "if", "then", "else",
-                   "while", "do", "not"]
-
+  reservedNames = ["program", "var", "integer", "real", "bolean", "procedure", "begin",
+                   "end", "if", "then", "else", "while", "do", "not"]
   delimiters = [",", "(", ")", ".", ":", ";"]
-
   relationalOp = [">", "<", "=", ">=", "<=", "<>"]
-
   addOp = ["+", "-"]
   mulOp = ["*", "/", "and"]
   
@@ -55,80 +43,67 @@ module Lexer(Line, Content, Delimiter, Keyword, Identifier,
     where
       loop :: String -> String -> Bool
       loop _ [] = False
-      loop r (n:ns) | n == '.'  = if (isNumberStr r) && (isNumberStr ns) 
-                                    then True 
-                                    else False
+      loop r (n:ns) | n == '.'  = if (isNumberStr $ r ++ ns) then True else False
                     | otherwise = loop (r++[n]) ns
 
-  mapType :: String -> Type
-  mapType [] = Null
-  mapType (x:[])  = if find [x] delimiters
-                      then Delimiter
-                    else Null
+  isIdentifier :: String -> Bool
+  isIdentifier ""     = False
+  isIdentifier (x:[]) = isAlphaNum x
+  isIdentifier (x:xs) = if isAlphaNum x || x == '_' then isIdentifier xs else False
+
+  mapType :: String -> Maybe Type
+  mapType [] = Nothing
   mapType x 
-          | find x delimiters     = Delimiter
-          | find x addOp          = AddOperator
-          | find x mulOp          = MulOperator
-          | find x relationalOp   = RelOperator
-          | find x reservedNames  = Keyword
-          | x == ":="             = Assignment
-          | isNumberStr x         = IntegerNumber
-          | isRealStr x           = RealNumber
-          | otherwise             = Identifier
+          | x == ":="             = Just Assignment
+          | find x delimiters     = Just Delimiter
+          | find x addOp          = Just AddOperator
+          | find x mulOp          = Just MulOperator
+          | find x relationalOp   = Just RelOperator
+          | find x reservedNames  = Just Keyword
+          | isNumberStr x         = Just IntegerNumber
+          | isRealStr x           = Just RealNumber
+          | isIdentifier x        = Just Identifier
+          | otherwise             = Nothing
 
   lexicalAnalysis :: String -> Maybe [Token]
   lexicalAnalysis []  = Nothing -- Undefined for empty input
-  lexicalAnalysis str = loop [] 1 Null "" str
+  lexicalAnalysis str = loop [] 1 (Just Null) "" str
     where
-      loop :: [Token] -> Integer -> Type -> String -> String -> Maybe [Token]
+      loop :: [Token] -> Integer -> Maybe Type -> String -> String -> Maybe [Token]
       loop tks n t "" ""             = Just tks
-      loop tks n t x ""              =  if mapType x == Null then
+      loop tks n t x ""              =  if isNothing(mapType x) then
                                           Nothing -- Undefined if the end of string is unknown
                                         else
                                           Just (tks ++ [(Token (x, t, n))])
 
-      loop tks n t x (' ':code)
-            | x == ""                = loop tks n t x code
-                | otherwise          = let t' = mapType x
-                                       in loop (tks ++ [(Token (x, t', n))]) n Null "" code
-
+      loop tks n t "" (' ':code)     = loop tks n t "" code
+      loop tks n t x  (' ':code)     = let t' = mapType x
+                                       in if (isJust t') then 
+                                            loop (tks ++ [(Token (x, t', n))]) n (Just Null) "" code
+                                          else Nothing
       -- If a linebreak is found, just increment line counter
-      loop tks n t x ('\n':code)     = loop (tks ++ [(Token (x, t, n))]) (n + 1) Null "" code
+      loop tks n t "" ('\n':code)    = loop tks (n + 1) t "" code
+      loop tks n t x ('\n':code)     = loop (tks ++ [(Token (x, t, n))]) (n + 1) (Just Null) "" code
       loop tks n t x ('{':code)      = loop' tks n t x code -- removing comments
         where
-          loop' :: [Token] -> Integer -> Type -> String -> String -> Maybe [Token]
+          loop' :: [Token] -> Integer -> Maybe Type -> String -> String -> Maybe [Token]
           loop' tks n t x ('}':code') = loop tks n t x code'
           loop' tks n t x (_:code')   = loop' tks n t x code'
           -- Undefined if curly brackets are not closed
           loop' tks n t x []          = Nothing
-
-      loop tks n t x ('.':code)       = if (isNumberStr x) &&
-                                          isDigit (head code)
-                                         then loop tks n RealNumber x code
-                                       else
-                                         loop (tks ++ [(Token (x, t, n))]) n Null "" code
+      loop tks n t "" (c:code)
+           | isJust(mapType [c])      = loop tks n (mapType [c]) [c] code
+           | otherwise                = do Nothing
       loop tks n t x (c:code)
-        | isAlphaNum c || c == '_'            
-                                     =   let t' = mapType (x ++ [c])
-                                           in if t' == Null then
-                                                if x == "" then
-                                                  loop tks n t' [c] code
-                                                else
-                                                  loop (tks ++ [(Token (x, t, n))]) n Null "" (c:code)
-                                              else
-                                                loop tks n t' (x ++ [c]) code   
-{-
-        | x /= "" && 
-          mapType [c] == Delimiter &&
-          mapType (x ++ [c]) == Identifier
-                                     = loop (tks ++ [(Token (x, t, n))]) n Delimiter [c] code
-        | x == "" &&
-          mapType [c] == Delimiter   = loop tks n Delimiter [c] code
-        | otherwise                  = Just tks-}
-
+        | isJust (mapType (x ++ [c])) = loop tks n (mapType (x++[c])) (x++[c]) code
+        | isJust (mapType x)          = loop (tks ++ [Token (x, mapType x, n)])
+                                           n (mapType[c]) [c] code
+        | otherwise                   = Nothing
+           
   showToken :: Maybe Token -> String
   showToken Nothing                  = ""
-  showToken (Just (Token (c, t, l))) = c ++ " " ++ (show t) ++ " " ++ (show l) ++ "\n"
+  showToken (Just (Token (c, t, l))) = c ++ " " ++ (show (fromJust t)) ++ " " ++ (show l) ++ "\n"
+                                       
 
   writeTokens :: String -> String
   writeTokens "" = "empty"
